9e35602b27c88175734eb8aa1f41884e463f229b
RandomName-proj <2012ame2012@gmail.com>
2023-03-26 11:51:05 +0300

    убрал иноязычные открытки

--- stable_diffusion_generator.py
+++ stable_diffusion_generator.py
@@ -12,10 +12,10 @@ class Generator:
     def get_images(self, text):
         num_rus, num_eng = 2, 2
         rus_promts = [text + ' high quality, high resolution, 4 k'] * num_rus
-        eng_prompts = [ts.translate_text(text) + ' high quality, high resolution, 4 k'] * num_eng
+        #eng_prompts = [ts.translate_text(text) + ' high quality, high resolution, 4 k'] * num_eng
         
         images = []
-        images.extend(self.pipe(eng_prompts).images)
+        #images.extend(self.pipe(eng_prompts).images)
         images.extend(self.pipe(rus_promts).images)
         return images
 
